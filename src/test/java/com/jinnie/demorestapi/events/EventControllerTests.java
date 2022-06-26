6bf9cd689cde8a44198753253ffd22af763e52e4
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-06-26 22:45:15 +0900

    Body for BadRequest

--- src/test/java/com/jinnie/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/jinnie/demorestapi/events/EventControllerTests.java
@@ -2,13 +2,12 @@ package com.jinnie.demorestapi.events;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jinnie.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -31,6 +30,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         Event event = Event.builder()
                 .name("Spring")
@@ -63,6 +63,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_request() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -85,6 +86,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_request_Empty_Input() throws Exception{
         EventDto eventDto = EventDto.builder().build();
 
@@ -95,6 +97,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_request_Wrong_Input() throws Exception{
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -112,6 +115,10 @@ public class EventControllerTests {
         this.mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(this.objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists());
     }
 }
