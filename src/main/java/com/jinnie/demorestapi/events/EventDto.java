09945e58537c3b778b364f01a30c1a44d39d2370
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-06-26 21:12:54 +0900

    fail on unknown properties

--- src/main/java/com/jinnie/demorestapi/events/EventDto.java
+++ src/main/java/com/jinnie/demorestapi/events/EventDto.java
@@ -2,10 +2,12 @@ package com.jinnie.demorestapi.events;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/test/java/com/jinnie/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/jinnie/demorestapi/events/EventControllerTests.java
@@ -4,7 +4,6 @@ package com.jinnie.demorestapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -60,5 +59,27 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_request() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,23,15,21))
+                .endEventDateTime(LocalDateTime.of(2028,11,26,14,26,25))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
