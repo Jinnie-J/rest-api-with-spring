8fc4688bf1586f3ebb75c5ead76e78dc76d94541
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-06-30 23:49:26 +0900

    spring hateoas

--- src/main/java/com/jinnie/demorestapi/events/EventController.java
+++ src/main/java/com/jinnie/demorestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package com.jinnie.demorestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.validation.Errors;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +44,11 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+        return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
