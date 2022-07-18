1f8f749bafb596badc05a6a1d8b63fe11b008572
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-07-18 23:21:09 +0900

    Test exceptions

--- src/test/java/com/jinnie/demorestapi/AccountServiceTest.java
+++ src/test/java/com/jinnie/demorestapi/AccountServiceTest.java
@@ -4,23 +4,31 @@ import com.jinnie.demorestapi.accounts.Account;
 import com.jinnie.demorestapi.accounts.AccountRepository;
 import com.jinnie.demorestapi.accounts.AccountRole;
 import com.jinnie.demorestapi.accounts.AccountService;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
+import static org.aspectj.bridge.MessageUtil.fail;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -46,5 +54,18 @@ public class AccountServiceTest {
 
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void findByUsernameFail(){
+        //Expected
+        String username= "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        //When
+        accountService.loadUserByUsername(username);
+
+
     }
 }
